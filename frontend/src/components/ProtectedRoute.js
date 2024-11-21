40f75a1b5991cf42c2eab2e98f2b5ca2efef3f1a
mintmangoslushie <r.dylan.christensen@gmail.com>
2024-11-21 16:52:13 -0500

    Update ProtectedRoute.js

--- frontend/src/components/ProtectedRoute.js
+++ frontend/src/components/ProtectedRoute.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
+import { isAuthenticated } from '../auth'; // Reuse auth function
 
 const ProtectedRoute = () => {
-  const token = localStorage.getItem('token');
-  return token ? <Outlet /> : <Navigate to="/login" />;
+    return isAuthenticated() ? <Outlet /> : <Navigate to="/login" />;
 };
 
 export default ProtectedRoute;
