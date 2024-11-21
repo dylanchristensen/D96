7696a14224538bf80cb65339920dac035618a7d3
mintmangoslushie <r.dylan.christensen@gmail.com>
2024-11-21 17:34:06 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.use('/chartData', chartDataRoutes);
 
 // MongoDB connection
 mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .connect(MONGO_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
